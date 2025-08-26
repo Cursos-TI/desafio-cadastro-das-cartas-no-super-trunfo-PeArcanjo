02e38e037a6c99d5755fbc0e03d16de6b3bafd51
Miguel Arcanjo de Oliveira Peroni <miguelarcanjop2018@gmail.com>
2025-08-26 21:49:35 +0000

    Criei as variáveis: char,int e float

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,5 +4,23 @@ int main(){
   
 printf("Desafio Super Trunfo - Países\n");
 
+// Dados da Carta 1 (Variáveis)
+    char estado1;
+    char codigo1[4];
+    char nomeCidade1[50];
+    int populacao1;
+    float area1;
+    float pib1;
+    int pontosTuristicos1;
+
+    // Dados da Carta 2 (Variáveis)
+    char estado2;
+    char codigo2[4];
+    char nomeCidade2[50];
+    int populacao2;
+    float area2;
+    float pib2;
+    int pontosTuristicos2;
+    
 return 0;
 }
